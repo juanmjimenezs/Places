8353b01d6f857a48c54c153f661670e322e4a831
Juan Manuel Jimenez Sanchez <juanmjimenezs@MacBook-Pro-de-Juan.local>
2016-12-12 18:44:50 -0500

    Se agregó una clase que funciona como una extensión de UIButton y sirve para configurarle ciertos parámetros que no son visibles fácilmente para los botones

---
+++ Places/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  Places
+//
+//  Created by Juan Manuel Jimenez Sanchez on 12/12/16.
+//  Copyright © 2016 Juan Manuel Jimenez Sanchez. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+}
